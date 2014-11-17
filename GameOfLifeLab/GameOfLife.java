b761681923aeba2d43e72ca904bad3380e565993
john chen <cheezyj123@gmail.com>
2014-11-17 17:07:10 -0600

    lab
    lab

--- GameOfLifeLab/GameOfLife.java
+++ GameOfLifeLab/GameOfLife.java
@@ -3,7 +3,8 @@ import info.gridworld.actor.ActorWorld;
 import info.gridworld.actor.Rock;
 import info.gridworld.grid.BoundedGrid;
 import info.gridworld.grid.Location;
-
+import info.gridworld.grid.Grid;
+import java.awt.Color;
 /**
  * Game of Life starter code. Demonstrates how to create and populate the game using the GridWorld framework.
  * Also demonstrates how to provide accessor methods to make the class testable by unit tests.
@@ -15,41 +16,36 @@ public class GameOfLife
 {
     // the grid of Actors that maintains the state of the game (alive cells contains actors; dead cells do not)
     private BoundedGrid<Actor> grid;
-    
+   
     // the world comprised of the grid that displays the graphics for the game
     private ActorWorld world;
-    
-    // the game board will have 5 rows and 5 columns
-    private final int ROWS = 5;
-    private final int COLS = 5;
-    
-    // constants for the location of the three cells initially alive
-    private final int X1 = 0, Y1 = 2;
-    private final int X2 = 2, Y2 = 0;
-    private final int X3 = 2, Y3 = 1;
 
+    // the game board will have 10 rows and 10 columns
+    private final int ROWS = 75;
+    private final int COLS = 75;
+    
     /**
      * Default constructor for objects of class GameOfLife
      * 
      * @post    the game will be initialized and populated with the initial state of cells
      * 
      */
-    public GameOfLife()
+    public GameOfLife() throws InterruptedException
     {
         // create the grid, of the specified size, that contains Actors
         grid = new BoundedGrid<Actor>(ROWS, COLS);
         
         // create a world based on the grid
         world = new ActorWorld(grid);
-        
+
         // populate the game
         populateGame();
-        
+
         // display the newly constructed and populated world
         world.show();
-        
+
     }
-    
+
     /**
      * Creates the actors and inserts them into their initial starting positions in the grid
      *
@@ -57,24 +53,60 @@ public class GameOfLife
      * @post    all actors that comprise the initial state of the game have been added to the grid
      * 
      */
-    private void populateGame()
+    private void populateGame() throws InterruptedException
     {
-        // create and add rocks (a type of Actor) to the three intial locations
-        Rock rock1 = new Rock();
-        Location loc1 = new Location(X1, Y1);
-        grid.put(loc1, rock1);
+        // create and add rocks (a type of Actor) to the thirty-something intial locations
+        grid.put(new Location(6, 1), new Rock());        
+        grid.put(new Location(6, 2), new Rock());        
+        grid.put(new Location(7, 1), new Rock());
+        grid.put(new Location(7, 2), new Rock());
+
+        grid.put(new Location(6, 11), new Rock());              
+        grid.put(new Location(7, 11), new Rock());        
+        grid.put(new Location(8, 11), new Rock());        
+        grid.put(new Location(5, 12), new Rock());
+        grid.put(new Location(9, 12), new Rock());        
+        grid.put(new Location(10, 14), new Rock());       
+        grid.put(new Location(10, 13), new Rock());        
+        grid.put(new Location(4, 14), new Rock());        
+        grid.put(new Location(4, 13), new Rock());        
+        grid.put(new Location(7, 15), new Rock());        
+        grid.put(new Location(5, 16), new Rock());        
+        grid.put(new Location(9, 16), new Rock());        
+        grid.put(new Location(6, 17), new Rock());       
+        grid.put(new Location(7, 17), new Rock());        
+        grid.put(new Location(8, 17), new Rock());        
+        grid.put(new Location(7, 18), new Rock());
+       
+        grid.put(new Location(4, 21), new Rock());
+        grid.put(new Location(5, 21), new Rock());
+        grid.put(new Location(6, 21), new Rock());
+        grid.put(new Location(4, 22), new Rock());
+        grid.put(new Location(5, 22), new Rock());
+        grid.put(new Location(6, 22), new Rock());       
+        grid.put(new Location(3, 23), new Rock());
+        grid.put(new Location(7, 23), new Rock());
+        grid.put(new Location(2, 25), new Rock());
+        grid.put(new Location(3, 25), new Rock());
+        grid.put(new Location(7, 25), new Rock());
+        grid.put(new Location(8, 25), new Rock());
         
-        Rock rock2 = new Rock();
-        Location loc2 = new Location(X2, Y2);
-        grid.put(loc2, rock2);
+        grid.put(new Location(4, 36), new Rock());
+        grid.put(new Location(5, 36), new Rock());
+        grid.put(new Location(4, 35), new Rock());
+        grid.put(new Location(5, 35), new Rock());
         
-        Rock rock3 = new Rock();
-        Location loc3 = new Location(X3, Y3);
-        grid.put(loc3, rock3);
+        
+        for (int i = 0; i <= 1000; i++)
+        {
+            this.createNextGeneration();
+            Thread.sleep(100);
+            world.show();
+        }
     }
 
     /**
-     * Returns the actor at the specified row and column. Intended to be used for unit testing.
+     * Returns the actor at the specified row and column from grid. Intended to be used for unit testing.
      *
      * @param   row the row (zero-based index) of the actor to return
      * @param   col the column (zero-based index) of the actor to return
@@ -84,7 +116,7 @@ public class GameOfLife
     public Actor getActor(int row, int col)
     {
         Location loc = new Location(row, col);
-        Actor actor = grid.get(loc);
+        Actor actor = this.world.getGrid().get(loc);
         return actor;
     }
 
@@ -97,7 +129,7 @@ public class GameOfLife
     {
         return ROWS;
     }
-    
+
     /**
      * Returns the number of columns in the game board
      *
@@ -107,15 +139,46 @@ public class GameOfLife
     {
         return COLS;
     }
-    
-    
+
     /**
-     * Creates an instance of this class. Provides convenient execution.
-     *
+     * creates and removes actors according to the rules of Conway's game of life
      */
-    public static void main(String[] args)
+    public  void createNextGeneration()
+    {
+        // create the grid of specified size, that contains actors
+        Grid<Actor> grid = world.getGrid();
+        BoundedGrid<Actor> grid2 = new BoundedGrid<Actor>(ROWS, COLS);
+        for (int r = 0; r < ROWS; r ++)
+        {
+            for (int c = 0; c < COLS; c++)
+            {
+                Location loca = new Location(r, c);
+                int neighbors = grid.getOccupiedAdjacentLocations(loca).size();
+                if (neighbors ==  3 && this.getActor(r, c) == null)
+                {
+                    Rock rock5 = new Rock();
+                    grid2.put(loca, rock5);
+                }
+                if (this.getActor(r, c) != null && (neighbors == 3 || neighbors == 2))
+                {
+                    Rock rock4 = new Rock();
+                    grid2.put(loca, rock4);
+                }
+            }
+        }
+        world.setGrid(grid2);
+    }
+
+    /**
+    * Creates an instance of this class. Provides convenient execution.
+    *
+    */
+    public static void main(String[] args) throws InterruptedException
     {
         GameOfLife game = new GameOfLife();
     }
 
 }
+
+
+
--- GameOfLifeLab/GameOfLifeTest.java
+++ GameOfLifeLab/GameOfLifeTest.java
@@ -41,16 +41,16 @@ public class GameOfLifeTest
     }
 
     @Test
-    public void testInitialState()
+    public void testInitialState() throws InterruptedException
     {
         /* expected pattern for initial state
          *  (X: alive; -: dead)
          * 
          *    0 1 2 3 4
-         *  0 - - X - -
-         *  1 - - - - -
-         *  2 X X - - -
-         *  3 - - - - -
+         *  0 - - - - -
+         *  1 - - X - -
+         *  2 - - X - -
+         *  3 - - X - -
          *  4 - - - - -
          *  
          */
@@ -67,9 +67,9 @@ public class GameOfLifeTest
                 Actor cell = game.getActor(row, col);
 
                 // if the cell at the current row and col should be alive, assert that the actor is not null
-                if(     (row == 0 && col == 2) ||
-                        (row == 2 && col == 0) ||
-                        (row == 2 && col == 1))
+                if(     (row == 2 && col == 1) ||
+                        (row == 2 && col == 2) ||
+                        (row == 3 && col == 2))
                 {
                     assertNotNull("expected alive cell at (" + row + ", " + col + ")", cell);
                 }
@@ -79,6 +79,53 @@ public class GameOfLifeTest
                 }
             }
         }
+    }
+    
+    @Test
+    public void testFinalState() throws InterruptedException
+    {
+        /*
+         * expected pattern after 3 generations
+         * 
+         *    0 1 2 3 4
+         *  0 - - - - -
+         *  1 - - - - -
+         *  2 - X X X -
+         *  3 - - - - -
+         *  4 - - - - -
+         */
+        
+        GameOfLife game = new GameOfLife();
+        final int ROWS = game.getNumRows();
+        final int COLS = game.getNumCols();
+        
+        //creates 3 generations
+        for (int i = 0; i <= 3; i++)
+        {
+            game.createNextGeneration();
+        }
+        
+        for(int row = 0; row < ROWS; row++)
+        {
+            for(int col = 0; col < COLS; col++)
+            {
+                // in this example, an alive cell has a non-null actor and a dead cell has a null actor
+                Actor cell = game.getActor(row, col);
+
+                // if the cell at the current row and col should be alive, assert that the actor is not null
+                if(     (row == 1 && col == 2) ||
+                        (row == 2 && col == 2) ||
+                        (row == 3 && col == 3))
+                {
+                    assertNotNull("expected alive cell at (" + row + ", " + col + ")", cell);
+                }
+                else // else, the cell should be dead; assert that the actor is null
+                {
+                    assertNull("expected dead cell at (" + row + ", " + col + ")", cell);
+                }
+            }
+        }
+
     }
 }
 
